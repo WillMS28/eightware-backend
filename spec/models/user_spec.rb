2a87a965b71ae905e8c1ec316b958e976152c76d
WillMS28 <silva.miranda.william@gmail.com>
2025-06-08 20:05:52 -0300

    test: add basic validations for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    user = User.new(email: "test@example.com", password: "123456")
+    expect(user).to be_valid
+  end
+
+  it "is not valid without an email" do
+    user = User.new(password: "123456")
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without a password" do
+    user = User.new(email: "test@example.com")
+    expect(user).to_not be_valid
+  end
 end
